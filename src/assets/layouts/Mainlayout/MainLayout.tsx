fa88d725224656fcb0cfd9f7fd273f82c3e96d94
ismaelnascimento <ismael.nascimento.dev@gmail.com>
2023-06-08 23:31:06 -0300

    organizando..

--- src/assets/layouts/Mainlayout/MainLayout.tsx
+++ src/assets/layouts/Mainlayout/MainLayout.tsx
@@ -1,30 +1,36 @@
-
-import React from 'react'
-import Sidebar from '../Sidebar/Sidebar'
-import Example from '../Modal/modal'
-import './MainLayout.css'
-import ListPatients from '../../../components/TablesList/listPatients/ListPatients'
-import Header from '../Header/Header'
+import React from "react";
+import Sidebar from "../Sidebar/Sidebar";
+import Example from "../Modal/modal";
+import "./MainLayout.css";
+import ListPatients from "../../../components/TablesList/listPatients/ListPatients";
+import Header from "../Header/Header";
+import Atendimento from "../Atendimento/Atendimento/InfoAtendimento";
+import FormExample from "../Atendimento/Atendimento/Formatendimento";
+import { Route, Routes } from "react-router-dom";
+import FormAtendimento from "../Atendimento/Atendimento/Formatendimento";
+import AtendimentoPage from "../../../pages/Atendimento";
+import Home from "../../../pages/Home";
+import Footer from "../Footer/Footer";
 
 function Mainlayout() {
   return (
-    <div className='tudo'>
-     <Sidebar/>
-     <div className='main'>
-     <Header/>
-   
-        <div className='main-content'>
-      
-          {/* <div className='content'> */}
-          <Example/>
-          <ListPatients/>
-          {/* </div> */}
-        
+    <div className="tudo">
+      <Sidebar />
+      <div className="main">
+        <Header />
+
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+
+            <Route path="/atendimento/:id" element={<AtendimentoPage />} />
+          </Routes>
         </div>
-        
-    </div> 
+
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Mainlayout
+export default Mainlayout;
